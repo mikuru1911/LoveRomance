5f43d1da08407418b48636d0e96604b2b28a3b45
mori_shotaro <mori_shotaro@plise.co.jp>
2025-07-08 15:01:32 +0900

    デザインの修正

--- index.js
+++ index.js
@@ -2,18 +2,32 @@ const processStart = () => {
   const text = document.getElementById('textInput').value;
   const output = document.getElementById('output');
   const loading = document.getElementById('loading');
+  const btn = document.getElementById("btn")
+  const wrapper = document.getElementById("wrapper")
 
   if (text.trim() === "") {
     output.innerText = "Please enter some text.";
     return;
   }
+  const date = new Date()
+  const timestamp = date.getTime()
 
-  loading.style.display = "inline-block";
+  const img = document.createElement("img")
+  img.src = "./images/cat_dance.gif?" + timestamp
+  img.style.width = "60px"
+  wrapper.textContent = null
+
+  // ボタンを押したらローディング猫が出る。
+  // 1秒後に消える
+  setTimeout(() => {
+    wrapper.appendChild(img)
+  }, 0)
+
   // ここが出力処理
   setTimeout(() => {
+    wrapper.textContent = null;
     let doc = nlp(text);
     let past = doc.sentences().toPastTense().text();
     output.innerText = past;
-    loading.style.display = "none";
-  }, 500);
+  }, 1000);
 }
