4dee701258c66c7620728cfad6028bf602a73fc7
mori_shotaro <mori_shotaro@plise.co.jp>
2025-07-08 14:13:26 +0900

    デザインとjsの基盤作成

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-function convertToPast()
-{
-    let text = document.getElementById('textInput').Value;
-    let output = document.getElementById('output');
-    let loading = document.getElementById('loading');
+const processStart = () => {
+  const text = document.getElementById('textInput').value;
+  const output = document.getElementById('output');
+  const loading = document.getElementById('loading');
 
-    if (text.trim() === "") {
+  if (text.trim() === "") {
     output.innerText = "Please enter some text.";
     return;
-    }
+  }
 
-    loading.style.display = "inline-block";
-    setTimeout(() => {
-      let doc = nlp(text);
-      let past = doc.sentences().toPastTense().text();
-      output.innerText = past;
-      loading.style.display = "none";
-    }, 500);
+  loading.style.display = "inline-block";
+  // ここが出力処理
+  setTimeout(() => {
+    let doc = nlp(text);
+    let past = doc.sentences().toPastTense().text();
+    output.innerText = past;
+    loading.style.display = "none";
+  }, 500);
 }
-   
